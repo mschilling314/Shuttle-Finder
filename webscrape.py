8a8f661315838789582dfa658258bfea8b732e47
mschilling314 <mschilling52019@gmail.com>
2023-04-19 02:16:30 -0500

    Scrape done?

--- webscrape.py
+++ webscrape.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
-def get_schedule(url: str, deposit: str) -> None:
+def get_schedule(url: str) -> pd.DataFrame:
     """
     Gets an up-to-date schedule assuming no changes in service, writes it to a 2D CSV.
 
@@ -16,9 +17,22 @@ def get_schedule(url: str, deposit: str) -> None:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     articles = soup.find_all('tr')
-    print(articles)
+    
+    stop_names = []
+    stop_times = []
+    for index in range(len(articles)):
+        row = articles.find_all('td')
+        stop_names.append(row[0].text.strip())
+        row.pop(0)
+        times = []
+        for item in row:
+            times.append(item.text.strip())
+        stop_times.append(times)
+    df = pd.DataFrame({"Stop": stop_names, "Time": stop_times})
+    df.set_index("Stop", inplace=True)
+    return df
 
 
 
 if __name__ == '__main__':
-    get_schedule
+    print(get_schedule("https://www.northwestern.edu/transportation-parking/shuttles/routes/schedules/intercampus-evanston-to-chicago.html"))
