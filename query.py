8beaef5f07049d4831b443da7694b6298a2688f5
mschilling314 <mschilling52019@gmail.com>
2023-04-19 12:23:44 -0500

    Remove bisect/tolist, just use Pandas

--- query.py
+++ query.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import constants as c
 import os
-import bisect
 
 
 def grab_data(direction: int):
@@ -9,36 +8,33 @@ def grab_data(direction: int):
 
 
 def make_query(df: pd.DataFrame, stop: str, time: int):
-    times = df[stop].tolist()
-    time_index = bisect.bisect_right(times, time)
+    time_index = df[stop].searchsorted(time, side='right')
     return time_index
 
 
 def find_next_shuttle(df: pd.DataFrame, stop: str, time: int):
-    times = df[stop].tolist()
-    return times[make_query(df=df, stop=stop, time=time)]
+    time_index = make_query(df, stop, time)
+    return df[stop][time_index]
 
 
 def find_departure_time_to_arrive_by(df: pd.DataFrame, source: str, dest: str, arr_time: int):
     time_index = make_query(df=df, stop=dest, time=arr_time)
-    src = df[source].tolist()
     if time_index > 0:
         time_index -= 1
-    return src[time_index]
+    return df[source][time_index]
 
 
 def find_arrival_time_if_leaving_at(df: pd.DataFrame, source: str, dest: str, depart_time: int):
     time_index = make_query(df=df, stop=source, time=depart_time)
-    dst = df[dest].tolist()
-    return dst[time_index]
+    return df[dest][time_index]
 
 
 def find_first(df: pd.DataFrame, stop: str):
-    return df[stop].tolist()[0]
+    return df[stop][0]
 
 
 def find_last(df: pd.DataFrame, stop: str):
-    return df[stop].tolist()[-1]
+    return df[stop][len(df[stop])-1]
 
 
 if __name__=="__main__":
