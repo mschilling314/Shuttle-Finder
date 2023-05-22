e42e5323b22e70a15cb75fdd16b8b8c7800deef6
Matthew Schilling <77081591+mschilling314@users.noreply.github.com>
2023-05-22 14:41:58 -0500

    Refactor out CLI stuff

--- query.py
+++ query.py
@@ -112,31 +112,6 @@ def find_last(df: pd.DataFrame, stop: str) -> int:
     return df[stop][len(df[stop])-1]
 
 
-def cli_input() -> dict:
-    """
-    Prompts a user for various inputs in order to make a structured query.
-
-    Output is structured as a dictionary with the following str keys:
-    fun (required): holds an integer from 0-4 inclusive, corresponding to a query
-    dir (required): holds either 0 or 1 as an integer, corresponds to the direction we're going in
-    src: str for a stop, corresponds to departure and arrival time queries
-    dst: str for a stop, corresponds to departure and arrival time queries
-    time: formatted int (hmm or hhmm) required for next, departure, arrival time queries
-    stop: str for a stop, corresponds to next, first, last queries
-    """
-    res = {}
-    res["dir"] = int(input("\nPlease enter 0 if going to Chicago, 1 if going to Evanston.\n"))
-    res["fun"] = int(input("\nPlease input:\n0 for next shuttle \n1 for Depart to Arrive by \n2 for Arrive Leaving at \n3 for First\n4 for last\n"))
-    if res["fun"] in {0, 3, 4}:
-        res["stop"] = input("\nWhat stop did you want to inquire about? ")
-    else:
-        res["src"] = input("\nWhat stop will you leave from?\n")
-        res["dst"] = input("\nWhat stop are you going to?\n")
-    if res["fun"] in {0, 1, 2}:
-        res["time"] = int(input("\nWhat time did you have in mind?\n"))
-    return res
-
-
 def query_for_time(**kwargs) -> int:
     """
     Using properly structured input, returns an integer corresponding to the time resulting from the query.
@@ -173,13 +148,3 @@ def query_for_time(**kwargs) -> int:
         raise ValueError("Invalid arguments given.")
     return time
 
-
-if __name__=="__main__":
-    cont = True
-    while cont:
-        structured_input = cli_input()
-        time = query_for_time(**structured_input)
-        print(f"\nThe time that results from your query is: {time}\n")
-        contin = input("Input 0 to quit, or any other key to continue. ")
-        if contin == "0":
-            cont = False
